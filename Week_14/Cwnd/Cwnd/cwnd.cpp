9bb061d24b2d13d9a250f9003c22843a6a316ac5
Rinko <1957880233@qq.com>
2019-06-09 08:47:07 +0800

    尝试读取结束，准备处理数据

--- Week_14/Cwnd/Cwnd/cwnd.cpp
+++ Week_14/Cwnd/Cwnd/cwnd.cpp
@@ -3,14 +3,17 @@
 #include<string>
 using namespace std;
 
+string myip = "172.27.133.233";			//本机IP地址
+string Seq = "Seq";
+string Ack = "Ack";
+string Syn = "[SYN]";
+string Len = "Len";
+
 int main(){
-	//FILE* fp;
-	//fp = fopen("liquanlong3.xlsx", "r");
-	string myip = "172.27.129.248";			//本机IP地址
 	ifstream fin;
 	ofstream fout;
 	fout.open("result", ios::out);
-	fin.open("liquanlong3.csv", ios::in);
+	fin.open("test_1.csv", ios::in);
 	if (!fout.is_open()){
 		cout << "fout open fail!" << endl;
 		return 0;
@@ -20,16 +23,54 @@ int main(){
 		return 0;
 	}
 	string temp;
-	getline(fin, temp);
-	cout << temp << endl;
-	int no, packet_len, seq, ack;
+	getline(fin, temp);						//首行
+
+	unsigned int no, len, packet_seq, packet_ack, packet_len;
 	double time;
-	string src_ip, dst_ip, protocol;
-	fin >> no >> time >> src_ip >> dst_ip >> protocol >> packet_len;
-	getline(fin, temp);
-	cout << no << endl << time << endl << src_ip << endl << dst_ip << endl << protocol << endl << packet_len << endl << temp << endl;
-	fin >> no >> time >> src_ip >> dst_ip >> protocol >> packet_len;
-	getline(fin, temp);
-	cout << no << endl << time << endl << src_ip << endl << dst_ip << endl << protocol << endl << packet_len << endl << temp << endl;
+	string src_ip, dst_ip, protocol, info, equal;
+	while (fin >> no >> time >> src_ip >> dst_ip >> protocol >> len){
+		//getline(fin, info);
+		if (src_ip == myip){				//TCP Segment，本机发出
+			while (fin >> temp){
+				if (temp == Syn){			//非ACK
+					getline(fin, info);
+					break;
+				}
+				if (temp == Seq){			//读到Seq
+					fin >> equal >> packet_seq;
+				}
+				else if (temp == Ack){		//读到Ack
+					fin >> equal >> packet_ack;
+				}
+				else if (temp == Len){		//读到Len，后面不去要再读
+					fin >> equal >> packet_len;
+					getline(fin, info);
+					break;
+				}
+			}
+			if (temp == Len){
+
+			}
+		}
+		else if (dst_ip == myip){			//ACK，对方发出
+			while (fin >> temp){
+				if (temp == Syn){			//非ACK
+					getline(fin, info);
+					break;
+				}
+				if (temp == Seq){			//读到Seq
+					fin >> equal >> packet_seq;
+				}
+				else if (temp == Ack){		//读到Ack，后面不去要再读
+					fin >> equal >> packet_ack;
+					getline(fin, info);
+					break;
+				}
+			}
+			if (temp == Len){
+
+			}
+		}
+	}
 	return 0;
 }
